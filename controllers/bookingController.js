ad743b87aedcaf8fb48a1f606761e4fc5325bd09
muzammil-gitdev <muzammil.uiit@gmail.com>
2026-07-26 17:29:13 +0500

    completed passeger Booking logic

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,17 +1,40 @@
-import { createBookingService } from "../services/bookingService.js";
+import mongoose, { mongo } from "mongoose";
+import {
+  createBookingService,
+  findAndUpdateService,
+} from "../services/bookingService.js";
 
 export async function createBooking(req, res) {
+  const session = await mongoose.startSession();
+  const { to, from, transitDat, vehicleNo, seatsBooked } = req.body;
   try {
-    console.log(req.body);
-    const response = await createBookingService(req.body);
+    session.startTransaction();
+    const bookingResponse = await createBookingService(req.body, session);
+    const updateTransitResponse = await findAndUpdateService(
+      to,
+      from,
+      transitDat,
+      vehicleNo,
+      seatsBooked,
+      session,
+    );
+    console.log(updateTransitResponse);
+    if (!updateTransitResponse) {
+      throw new Error("Seat Already Booked");
+    }
+    await session.commitTransaction();
+
     res.status(200).json({
       success: true,
-      data: response,
+      message: "Data Submitted Successfully",
     });
   } catch (error) {
+    await session.abortTransaction();
     res.status(500).json({
       success: false,
       err: error.message,
     });
+  } finally {
+    session.endSession();
   }
 }
--- services/bookingService.js
+++ services/bookingService.js
@@ -1,5 +1,35 @@
 import bookingModel from "../models/bookingModel.js";
+import transitModel from "../models/trasitModel.js";
 
-export async function createBookingService(data) {
-  return await bookingModel.create(data);
+export async function createBookingService(data, session) {
+  return await bookingModel.create([data], { session });
+}
+export async function findAndUpdateService(
+  find_to,
+  find_from,
+  find_transitDat,
+  find_vehicleNo,
+  attach_seatsBooked,
+  session,
+) {
+  // console.log(find_from);
+  // console.log(find_to);
+  // console.log(find_transitDat);
+  // console.log(find_vehicleNo);
+  console.log(attach_seatsBooked);
+  return await transitModel.findOneAndUpdate(
+    {
+      to: find_to,
+      from: find_from,
+      departure: find_transitDat,
+      vehicleNumber: find_vehicleNo,
+      seatsBooked: { $nin: attach_seatsBooked },
+    },
+    { $push: { bookedSeats: { $each: attach_seatsBooked } } },
+    {
+      returnDocument: "after",
+      runValidators: true,
+      session,
+    },
+  );
 }
