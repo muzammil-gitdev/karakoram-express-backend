ef019b021c77587dc67b7baea109c3300ee6efe6
muzammil-gitdev <muzammil.uiit@gmail.com>
2026-08-15 18:39:20 +0500

    implemented passenger list on google

--- controllers/passegerController.js
+++ controllers/passegerController.js
@@ -4,14 +4,13 @@ export async function getPassengers(req, res) {
   try {
     const obj = Object.assign({}, req.query);
     console.log(obj);
-    console.log(typeof obj.transitDat);
     const { to, from, transitDat, vehicleNo } = obj;
     const passenger = await bookingModel.find({
       to,
       from,
-      transitDat,
       vehicleNo,
     });
+    console.log(passenger);
     res.status(200).json({
       success: true,
       passenger,
