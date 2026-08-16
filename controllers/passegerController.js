690c946f0d16d0f453a18294170767e936330ad3
muzammil-gitdev <muzammil.uiit@gmail.com>
2026-08-16 23:53:34 +0500

    fixed bugs in passenger list date

--- controllers/passegerController.js
+++ controllers/passegerController.js
@@ -2,13 +2,40 @@ import bookingModel from "../models/bookingModel.js";
 
 export async function getPassengers(req, res) {
   try {
+    console.log(req.query);
     const obj = Object.assign({}, req.query);
-    console.log(obj);
+    const searchDate = new Date(obj.transitDat);
+    const startOfDay = new Date(
+      Date.UTC(
+        searchDate.getUTCFullYear(),
+        searchDate.getUTCMonth(),
+        searchDate.getUTCDate(),
+        0,
+        0,
+        0,
+        0,
+      ),
+    );
+    const endOfDay = new Date(
+      Date.UTC(
+        searchDate.getUTCFullYear(),
+        searchDate.getUTCMonth(),
+        searchDate.getUTCDate() + 1,
+        0,
+        0,
+        0,
+        0,
+      ),
+    );
     const { to, from, transitDat, vehicleNo } = obj;
     const passenger = await bookingModel.find({
       to,
       from,
       vehicleNo,
+      transitDat: {
+        $gte: startOfDay,
+        $lt: endOfDay,
+      },
     });
     console.log(passenger);
     res.status(200).json({
